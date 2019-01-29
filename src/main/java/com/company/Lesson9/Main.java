ecc0e29f89a4bd0da909fe61a3c59c9eb9e23a3f
Anastasiia_Yarova <nastyaposhta@gmail.com>
2019-01-29 18:07:01 +0200

    + lesson9

---
+++ src/main/java/com/company/Lesson9/Main.java
@@ -0,0 +1,23 @@
+package com.company.Lesson9;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("hi");
+        System.setProperty("webdriver.chrome.driver", "drivers\\chromedriver.exe");
+
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.google.com/");
+
+        WebElement element = driver.findElement(By.xpath("//input[@title = 'Пошук']"));
+        element.sendKeys("Who is Zarina?");
+        element.sendKeys(Keys.RETURN);
+        driver.quit();
+    }
+}
