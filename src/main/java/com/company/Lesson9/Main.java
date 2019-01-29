32845275159f4fe8383c2ffdec8913702852946a
Anastasiia_Yarova <nastyaposhta@gmail.com>
2019-01-29 18:15:52 +0200

    + lesson9

--- src/main/java/com/company/Lesson9/Main.java
+++ src/main/java/com/company/Lesson9/Main.java
@@ -9,7 +9,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("hi");
+        System.out.println("hallo");
         System.setProperty("webdriver.chrome.driver", "drivers\\chromedriver.exe");
 
         WebDriver driver = new ChromeDriver();
