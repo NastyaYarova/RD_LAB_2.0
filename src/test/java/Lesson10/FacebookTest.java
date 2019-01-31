67f28c732ed2539ab156e229c39abb7ee9c2365b
Anastasiia_Yarova <nastyaposhta@gmail.com>
2019-01-31 12:52:52 +0200

    + lesson10

---
+++ src/test/java/Lesson10/FacebookTest.java
@@ -0,0 +1,49 @@
+package Lesson10;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class FacebookTest {
+
+    public void testLogPass(){
+        System.setProperty("webdriver.chrome.driver", "drivers\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+
+        driver.get("https://www.google.com/");
+
+        WebElement search = driver.findElement(By.xpath("//input[@class='gLFyf gsfi']"));
+        search.sendKeys("facebook");
+        search.sendKeys(Keys.RETURN);
+
+        WebElement findfacebook = driver.findElement(By.xpath("//a[@href='https://uk-ua.facebook.com/']//cite[@class='iUh30']"));
+        findfacebook.click();
+
+        WebElement findMail = driver.findElement(By.xpath("//input[@ id='email']"));
+        findMail.sendKeys("+380980261901");
+
+        WebElement findPass = driver.findElement(By.xpath("//input[@ id='pass']"));
+        findPass.sendKeys("11111111");
+
+        WebElement buttonLogin = driver.findElement(By.xpath("//input[@id='u_0_2']"));
+        buttonLogin.sendKeys(Keys.RETURN);
+
+        WebElement elementError = driver.findElement(By.xpath("//div[@class='_4rbf _53ij']"));
+
+        driver.quit();
+    }
+
+    public static void main(String[] args) {
+
+        FacebookTest testLogPass = new FacebookTest();
+        testLogPass.testLogPass();
+
+    }
+}
